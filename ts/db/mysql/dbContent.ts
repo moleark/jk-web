4acca38fbfb47ee1ef7c923effc81ecd47fb44ce
Henry <liaohengyi@outlook.com>
2020-02-14 17:52:43 +0800

    remove header

--- ts/db/mysql/dbContent.ts
+++ ts/db/mysql/dbContent.ts
@@ -8,11 +8,13 @@ export class DbContent extends DbBase {
         super('content');
         let db = this.databaseName;
         this.sqlHomePostList = `
-            SELECT a.id, a.caption, a.discription as disp, c.path as image, a.$update as date
+            SELECT a.id, a.caption, a.discription as disp, c.path as image,
+                a.$update as date, d.hits, d.sumHits
             FROM -- ${db}.tv_customerpost cp join ${db}.tv_post a on cp.post=a.id
                 ${db}.tv_post a 
                 left join ${db}.tv_template b on a.template=b.id 
                 left join ${db}.tv_image c on a.image=c.id
+                left join ${db}.tv_hot d on a.id=d.post
             ORDER BY a.id desc
             LIMIT 10;
         `;
