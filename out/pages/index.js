e86194c42c0ffd0fcc4b5924a2b51199c9110a77
wangyancai666 <ycai@jk-sci.com>
2020-04-09 19:11:50 +0800

    修改

--- out/pages/index.js
+++ out/pages/index.js
@@ -24,6 +24,7 @@ exports.homeRouter = express_1.Router({ mergeParams: true });
 exports.homeRouter.get('/', home_1.home);
 exports.homeRouter.get('/post/:id', post_1.post);
 exports.homeRouter.get('/category/:current', category_1.category);
+exports.homeRouter.get('/productcategory/:current', category_1.category);
 exports.homeRouter.get('/search/:key', search_1.search);
 exports.homeRouter.get('/search', search_1.search);
 exports.homeRouter.get('/product/:id', product_1.product);
