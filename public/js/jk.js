9b1fd1977a8f9f444812b8d042a48f222df3e101
wangyancai666 <ycai@jk-sci.com>
2020-04-16 15:27:08 +0800

    无法打开文献连接

---
+++ public/js/jk.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    let ismobel = browserRedirect();
+    alert(ismobel);
+    if (!ismobel) {
+        $("li a").each(function (index, element) {
+            $(element).attr("target", "_blank");
+        });
+    }
+}
+
+function browserRedirect() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(sUserAgent)) {
+        return true;
+    } else {
+        return false;
+    }
+}
