d21f77df7fc961a30c909d4feff913829f6ba331
ligsh <system6@jkchemical.com>
2020-08-07 19:23:56 +0800

    ligsh, hattie提供的产品目录树及列表界面

--- public/hattie/vendor/jquery/jk.js
+++ public/hattie/vendor/jquery/jk.js
@@ -18,4 +18,13 @@ $(function(){
         $(".ahover2").click(function(){
         $(".stable2").slideToggle("1000");
         });
+        $(".ahover3").click(function(){
+        $(".stable3").slideToggle("1000");
+        });
+        $(".ahover4").click(function(){
+        $(".stable4").slideToggle("1000");
+        });
+        $(".ahover5").click(function(){
+        $(".stable5").slideToggle("1000");
+        });
       });
