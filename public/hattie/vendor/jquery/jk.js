28ba43fd3d99951bbf4cc9f562cd6e202c6c5b6f
shuhanyang <hattie.yang25@gmail.com>
2020-09-08 23:30:17 -0400

    update

--- public/hattie/vendor/jquery/jk.js
+++ public/hattie/vendor/jquery/jk.js
@@ -1,17 +1,6 @@
+
 $(function(){
-    $(".dropdown").hover(            
-            function() {
-                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
-                $(this).toggleClass('open');
-                $('b', this).toggleClass("caret caret-up");                
-            },
-            function() {
-                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
-                $(this).toggleClass('open');
-                $('b', this).toggleClass("caret caret-up");                
-            });
-    });
-    $(document).ready(function(){
+
         $(".ahover").click(function(){
         $(".stable").slideToggle("1000");
         });
@@ -26,5 +15,6 @@ $(function(){
         });
         $(".ahover5").click(function(){
         $(".stable5").slideToggle("1000");
-        });
-      });
+        });   
+   
+});
