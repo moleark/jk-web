7133e84d71bd440f74b6ce192b54ddc1ad1c2788
ZWGKing <nage@163.com>
2020-11-17 14:14:20 +0800

    zwg,U,1.获取PDF文件路径

--- ts/pages/ProductMSCU/getFilePath.ts
+++ ts/pages/ProductMSCU/getFilePath.ts
@@ -1,24 +1,12 @@
 import * as config from 'config';
 
-export const o = process.env.NODE_ENV === 'production'
-    ? {
-        196: "CN",
-        38: "EN",
-        35: "DE",
-        56: "EN-US",
-    }
-    : {
-        197: "CN",
-        52: "EN",
-        32: "DE",
-        55: "EN-US",
-    }
-
 export function getFilePath(type: string, fileName: string, lang?: number | string) {
+    let o = config.get('FILELANGVER');
     let fileAscr = o[lang] ? o[lang] : (fileName.includes('_EN') ? 'EN' : 'CN');
     let MSCUPath = config.get("MSCUPath");
-    if (process.env.NODE_ENV === 'production')
+    return `${MSCUPath}${type}/${fileAscr}/${fileName}`;
+    /* if (process.env.NODE_ENV === 'production')
         return `${MSCUPath}${type}/${fileAscr}/${fileName}`;
     else
-        return MSCUPath + fileName;//'100008_CN.PDF' 
+        return MSCUPath + fileName;//'100008_CN.PDF' */
 }
