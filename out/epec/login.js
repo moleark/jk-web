554ec31814b94eb6b470e2cc893dde03ac81fcf2
ligsh <system6@jkchemical.com>
2021-04-15 15:45:34 +0800

    ligsh, epec login 添加log及回访epec时不验证其证书

--- out/epec/login.js
+++ out/epec/login.js
@@ -15,9 +15,11 @@ const config = require("config");
 const node_fetch_1 = require("node-fetch");
 const uuid_1 = require("uuid");
 const getUserRegisted_1 = require("../tools/getUserRegisted");
+const https_1 = require("https");
 function login(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        let { query } = req;
+        let { originalUrl, query } = req;
+        console.debug(originalUrl);
         let { account, onlyCode } = query;
         if (!account || !onlyCode) {
             res.redirect("/login");
@@ -32,24 +34,35 @@ function login(req, res) {
         // 调用epec接口验证
         let epecOptions = config.get('epec');
         let { epec_loginCallBack, epec_loginSuccessRedirect } = epecOptions;
-        let response = yield node_fetch_1.default(epec_loginCallBack);
-        if (response.ok) {
-            let content = yield response.json();
-            if (content.result) {
-                // OK
-                // 记录此次登录请求，并使用此登录请求的id实现在客户端的再次验证
-                let token = uuid_1.v4();
-                let { webUser, password, username } = epecUser;
-                let userInfo = yield getUserRegisted_1.getUserRegisted(webUser);
-                if (userInfo) {
-                    let success = yield jointPlatform.saveLoginReq(token, userInfo.name, password, username);
-                    // 导航到默认界面
-                    if (success) {
-                        res.redirect(epec_loginSuccessRedirect + "?lgtk=" + token);
-                        return;
+        try {
+            let response = yield node_fetch_1.default(epec_loginCallBack + `?account=${account}&onlyCode=${onlyCode}`, {
+                method: 'GET',
+                headers: { "Content-Type": "application/json" },
+                agent: new https_1.default.Agent({ rejectUnauthorized: false })
+            });
+            console.debug('epec login call back status', response.status);
+            if (response.ok) {
+                let content = yield response.json();
+                console.debug('epec login call back content: ', content);
+                if (content.result) {
+                    // OK
+                    // 记录此次登录请求，并使用此登录请求的id实现在客户端的再次验证
+                    let token = uuid_1.v4();
+                    let { webUser, password, username } = epecUser;
+                    let userInfo = yield getUserRegisted_1.getUserRegisted(webUser);
+                    if (userInfo) {
+                        let success = yield jointPlatform.saveLoginReq(token, userInfo.name, password, username);
+                        // 导航到默认界面
+                        if (success) {
+                            res.redirect(epec_loginSuccessRedirect + "?lgtk=" + token);
+                            return;
+                        }
                     }
                 }
             }
+        }
+        catch (error) {
+            console.error('epec login call back error: ', error);
         }
         res.redirect("/login");
     });
--- ts/epec/login.ts
+++ ts/epec/login.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
 import { Dbs } from "../db";
-import * as config from 'config';
+import c, * as config from 'config';
 import fetch from "node-fetch";
 import { v4 as uuidv4 } from 'uuid';
 import { getUserRegisted } from "../tools/getUserRegisted";
+import https from 'https';
 
 export async function login(req: Request, res: Response) {
 
-    let { query } = req;
+    let { originalUrl, query } = req;
+    console.debug(originalUrl);
     let { account, onlyCode } = query;
     if (!account || !onlyCode) {
         res.redirect("/login");
@@ -25,24 +27,34 @@ export async function login(req: Request, res: Response) {
     let epecOptions = config.get<any>('epec');
     let { epec_loginCallBack, epec_loginSuccessRedirect } = epecOptions;
 
-    let response = await fetch(epec_loginCallBack);
-    if (response.ok) {
-        let content = await response.json();
-        if (content.result) {
-            // OK
-            // 记录此次登录请求，并使用此登录请求的id实现在客户端的再次验证
-            let token = uuidv4();
-            let { webUser, password, username } = epecUser;
-            let userInfo = await getUserRegisted(webUser);
-            if (userInfo) {
-                let success = await jointPlatform.saveLoginReq(token, userInfo.name, password, username);
-                // 导航到默认界面
-                if (success) {
-                    res.redirect(epec_loginSuccessRedirect + "?lgtk=" + token);
-                    return;
+    try {
+        let response = await fetch(epec_loginCallBack + `?account=${account}&onlyCode=${onlyCode}`, {
+            method: 'GET',
+            headers: { "Content-Type": "application/json" },
+            agent: new https.Agent({ rejectUnauthorized: false })
+        });
+        console.debug('epec login call back status', response.status);
+        if (response.ok) {
+            let content = await response.json();
+            console.debug('epec login call back content: ', content);
+            if (content.result) {
+                // OK
+                // 记录此次登录请求，并使用此登录请求的id实现在客户端的再次验证
+                let token = uuidv4();
+                let { webUser, password, username } = epecUser;
+                let userInfo = await getUserRegisted(webUser);
+                if (userInfo) {
+                    let success = await jointPlatform.saveLoginReq(token, userInfo.name, password, username);
+                    // 导航到默认界面
+                    if (success) {
+                        res.redirect(epec_loginSuccessRedirect + "?lgtk=" + token);
+                        return;
+                    }
                 }
             }
         }
+    } catch (error) {
+        console.error('epec login call back error: ', error);
     }
     res.redirect("/login");
 }
